9532bb9500452aa3a84f6df323d5fa9fc1c133c9
anilkamatham <anil.kamatham@gmail.com>
2020-12-03 16:08:41 +0530

    first commit Test.java

---
+++ src/com/webapp/clone/remote/Test.java
@@ -0,0 +1,10 @@
+package com.webapp.clone.remote;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("cloned empty remote repo and created the project locally");
+
+	}
+
+}
